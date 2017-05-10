767f9486b5bacb709a46d9cc002121e876424a25
jannikzgraggen <jannik@tsf-460-wpa-0-101.epfl.ch>
2017-05-10 11:21:26 +0200

    BC

--- A_CostCalc_MinMax_BC.m
+++ A_CostCalc_MinMax_BC.m
@@ -5,7 +5,7 @@ k2B = 0.738;
 PB =10.^(k1B + k2B*log10(duty))
 
 %% HEAT PUMP
-capacity= [1500,12000]
+capacity= [13000,12000]
 PHP =14901.0.*(capacity./95).^0.9
 
 %% SOFC
@@ -19,7 +19,7 @@ CAPICE = [100,15000]
 PICE = 10.^(k1I + k2I*log10(CAPICE) + k3I.*(log10(CAPICE)).^2)
 PICE2 = -0.0266.*(3*CAPICE/4).^2 + 578.84*(3*CAPICE/4) + 208174
 %% Turbine
-CAPGT = [100,15000]
+CAPGT = [10000,15000]
 k1G=3.4171;
 k2G=0.6112;
 PGT = 10.^(k1G + k2G.*log10(CAPGT))
