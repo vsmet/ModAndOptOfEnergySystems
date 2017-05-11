123ca5756ce9fed6701f93806b0b53988d524d7f
vsmet <vincent.smet@epfl.ch>
2017-05-11 15:31:41 +0200

    Revert "BC"
    This reverts commit 767f9486b5bacb709a46d9cc002121e876424a25.

--- A_CostCalc_MinMax.m
+++ A_CostCalc_MinMax.m
@@ -1,24 +1,22 @@
-%% BOILER
+%HEAT PUMP
+capacity= [1500,25000]
+PHP =14901.0.*(capacity./95).^0.9
+%BOILER
 duty = [1000,25000]
 k1B = 2.291;
 k2B = 0.738;
 PB =10.^(k1B + k2B*log10(duty))
-
-%% HEAT PUMP
-capacity= [1500,12000]
-PHP =14901.0.*(capacity./95).^0.9
-
-%% SOFC
-Cap = [100,10000]
-PSOFC = ((1.9*0.95)^.95)*1.02*1000*Cap % Not used in Code
-%% ICE
+%SOFC
+Cap = [1000,10000]
+PSOFC = ((1.9*0.95)^.95)*1.02*1000*Cap
+%ICE
 k1I = 2.7635;
 k2I = 0.8574;
 k3I = -0.0098;
 CAPICE = [100,15000]
 PICE = 10.^(k1I + k2I*log10(CAPICE) + k3I.*(log10(CAPICE)).^2)
 PICE2 = -0.0266.*(3*CAPICE/4).^2 + 578.84*(3*CAPICE/4) + 208174
-%% Turbine
+%Turbine
 CAPGT = [100,15000]
 k1G=3.4171;
 k2G=0.6112;
